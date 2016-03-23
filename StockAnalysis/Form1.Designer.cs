fe9cf0896816c6e44eae0845c91d3c42878fb10a
Faran Malik <faran2093@gmail.com>
2016-03-22 20:40:48 -0500

    Initial StockAnalysis Project Version

---
+++ StockAnalysis/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace StockAnalysis
+{
+    partial class StockAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.download = new System.Windows.Forms.Button();
+            this.location = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.stockSymb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.order = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // download
+            // 
+            this.download.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.download.Location = new System.Drawing.Point(6, 66);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(96, 23);
+            this.download.TabIndex = 0;
+            this.download.Text = "Download Data";
+            this.download.UseVisualStyleBackColor = true;
+            this.download.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // location
+            // 
+            this.location.AutoSize = true;
+            this.location.ForeColor = System.Drawing.Color.White;
+            this.location.Location = new System.Drawing.Point(134, 71);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(99, 13);
+            this.location.TabIndex = 1;
+            this.location.Text = "Download Location";
+            this.location.Click += new System.EventHandler(this.location_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkSlateGray;
+            this.dataGridView1.ForeColor = System.Drawing.Color.Black; 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(826, 171);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // stockSymb
+            // 
+            this.stockSymb.Location = new System.Drawing.Point(81, 27);
+            this.stockSymb.Name = "stockSymb";
+            this.stockSymb.Size = new System.Drawing.Size(100, 20);
+            this.stockSymb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Symbol:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.stockSymb);
+            this.groupBox1.Controls.Add(this.location);
+            this.groupBox1.Controls.Add(this.download);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(12, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(926, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Stock Info";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.order);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(12, 127);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(926, 248);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Historical Data";
+            // 
+            // order
+            // 
+            this.order.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.order.Location = new System.Drawing.Point(9, 33);
+            this.order.Name = "order";
+            this.order.Size = new System.Drawing.Size(75, 23);
+            this.order.TabIndex = 3;
+            this.order.Text = "Ascending ";
+            this.order.UseVisualStyleBackColor = true;
+            this.order.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // StockAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(950, 554);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "StockAnalysis";
+            this.Text = "Stock Analytics";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button download;
+        private System.Windows.Forms.Label location;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox stockSymb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button order;
+    }
+}
+
---
+++ StockAnalysis/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net; // Needed for web client
+using System.IO; // This is necessary for file streaming 
+
+namespace StockAnalysis
+{
+    public partial class StockAnalysis : Form
+    {
+        int buttonClick;
+        string[] myArr; 
+        WebClient wc = new WebClient(); // needed to download file 
+        public StockAnalysis()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog(); // prompts user to select folder
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                location.Text = fbd.SelectedPath;
+                String websiteFirst = "http://real-chart.finance.yahoo.com/table.csv?s=";
+                String websiteSecond = "&d=2&e=21&f=2016&g=d&a=5&b=29&c=2010&ignore=.csv";
+                String stock = stockSymb.Text; //"TSLA";
+                Uri url = new Uri(websiteFirst+stock+websiteSecond); 
+                wc.DownloadFile(url, location.Text + @"\finance_" +stock +".csv"); // Can be used to download files. Saves location and FileName  
+
+                FileStream fs = new FileStream(location.Text + @"\finance_" + stock + ".csv", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                DataSet dataset = new DataSet();
+                dataset.Tables.Add("FinData");
+                dataset.Tables["FinData"].Columns.Add("Date");
+                dataset.Tables["FinData"].Columns.Add("Open");
+                dataset.Tables["FinData"].Columns.Add("High");
+                dataset.Tables["FinData"].Columns.Add("Low");
+                dataset.Tables["FinData"].Columns.Add("Close");
+                dataset.Tables["FinData"].Columns.Add("Volume");
+                dataset.Tables["FinData"].Columns.Add("Adj Close");
+
+                string item; 
+                while((item = sr.ReadLine()) != null){
+                    myArr = item.Split(',');
+                    dataset.Tables["FinData"].Rows.Add(myArr); 
+                }
+                dataGridView1.DataSource = dataset.Tables[0].DefaultView;
+                dataGridView1.Rows.Remove(dataGridView1.Rows[0]);
+                dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void location_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            buttonClick++;
+            if (buttonClick%2 == 0)
+            {
+                dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Descending);
+                order.Text = "Ascending";
+            }
+            else if(buttonClick%2 != 0)
+            {
+                dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+                order.Text = "Descending";
+
+            }
+
+
+        }
+    }
+}
